3d5d201168b8685ec1af855d14cad814bfb08cde
kxxt <rsworktech@outlook.com>
2025-06-28 20:31:43 +0800

    Add dirty status to tree status
    Change-Id: I32b4e63817a1bef282e76927ea95e11da5e59d5b

--- format-repo-commit.sh
+++ format-repo-commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo -n "- \`$REPO_PATH\`: \`"
 git rev-parse --short=12 m/lineage-22.2 | tr -d $'\n'
-echo -n '`'
+echo -n '` '
+git status --porcelain=v1 | "$SCRIPT_DIR/git-dirty-status.rb"
---
+++ git-dirty-status.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'set'
+
+untracked = Set.new()
+deleted = Set.new()
+modified = Set.new()
+other = Set.new()
+
+ARGF.each do |line|
+    status, file = ( line.strip || line ).split(' ', 2)
+    case status
+    in '??'
+        untracked << file
+    in 'M'
+        modified << file
+    in 'D'
+        deleted << file
+    else
+        other << file
+    end
+end
+
+def markdown_code(input)
+    escaped = input.gsub('`') { '\`' }
+    return "`#{escaped}`"
+end
+
+clean = untracked.empty? && deleted.empty? && modified.empty? && other.empty?
+if ! clean
+    status = []
+    { "Modified" => modified, "Untracked" => untracked, "Deleted" => deleted, "Other" => other }.each do |key, value|
+        if ! value.empty? then
+            status.append("#{key}: " + value.map(&method(:markdown_code)).join(', '))
+        end
+    end
+    modified = modified.empty?? '' : "Modified: " + modified.map(&method(:markdown_code)).join(', ')
+    untracked =  untracked.empty?? '' : "Untracked: " + untracked.map(&method(:markdown_code)).join(', ')
+    deleted = deleted.empty?? '' : "Deleted: " + deleted.map(&method(:markdown_code)).join(', ')
+    other = other.empty?? '' : "Other: " + other.map(&method(:markdown_code)).join(', ')
+    status = status.join ', '
+    print "(Dirty, #{status})"
+end
