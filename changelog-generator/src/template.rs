8acb7206cfcd2ed74453eecc57ee29afa20108fe
kxxt <rsworktech@outlook.com>
2025-07-06 23:23:18 +0800

    changelog-generator: shorten commit hash
    Change-Id: I0bfad76978b6eecb5c41c1ecfb7205a4972d8f28

--- changelog-generator/src/template.rs
+++ changelog-generator/src/template.rs
@@ -2,7 +2,7 @@
 
 use handlebars::{
     Context, Handlebars, Helper, HelperResult, JsonRender, Output, RenderContext, RenderError,
-    RenderErrorReason, handlebars_helper,
+    RenderErrorReason,
 };
 
 use crate::changelog::ChangeLog;
@@ -59,9 +59,52 @@ fn indent(
     Ok(())
 }
 
+fn slice(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    rc: &mut RenderContext,
+    out: &mut dyn Output,
+) -> HelperResult {
+    let start = h
+        .param(0)
+        .ok_or_else(|| RenderErrorReason::ParamNotFoundForIndex("slice", 0))?
+        .try_get_constant_value()
+        .ok_or_else(|| RenderErrorReason::InvalidParamType("expected an index (integer)"))?
+        .as_u64()
+        .ok_or_else(|| RenderErrorReason::InvalidParamType("expected an index (integer)"))?
+        as usize;
+    let max_len = h
+        .param(1)
+        .ok_or_else(|| RenderErrorReason::ParamNotFoundForIndex("slice", 1))?
+        .try_get_constant_value()
+        .ok_or_else(|| RenderErrorReason::InvalidParamType("expected length (integer)"))?
+        .as_u64()
+        .ok_or_else(|| RenderErrorReason::InvalidParamType("expected length (integer)"))?
+        as usize;
+    let content = h
+        .param(2)
+        .ok_or_else(|| RenderErrorReason::ParamNotFoundForIndex("slice", 2))?
+        .render();
+    if content.is_empty() {
+        return Ok(());
+    }
+    if start >= content.len() {
+        return Err(RenderErrorReason::Other(format!(
+            "index {start} is out of range {}",
+            content.len()
+        ))
+        .into());
+    }
+    let end = (start + max_len).clamp(0, content.len().saturating_sub(1));
+    out.write(&content[start..end])?;
+    Ok(())
+}
+
 pub fn format_changelog(template: String, changelog: &ChangeLog) -> Result<String, RenderError> {
     let mut reg = Handlebars::new();
     reg.register_helper("md_link_desc_escape", Box::new(md_link_desc_escape));
     reg.register_helper("indent", Box::new(indent));
+    reg.register_helper("slice", Box::new(slice));
     reg.render_template(&template, &changelog)
 }
