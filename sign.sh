8279389018fdf55945ff41f4c60d3ec256733e94
Levi Zim <rsworktech@outlook.com>
2026-01-17 11:43:52 +0800

    sign.sh: add support for 23.2
    Change-Id: I1e87c241e88b956689b4a39f1d1168d85cfcbc33

--- sign.sh
+++ sign.sh
@@ -84,6 +84,36 @@ case "$LINEAGE_VERSION" in
             com.android.hardware.threadnetwork com.android.hardware.uwb com.android.hardware.vibrator com.android.uprobestats
         )
         ;;
+    23.2)
+        apexapks+=(
+            AdServicesApk FederatedCompute HalfSheetUX HealthConnectBackupRestore HealthConnectController
+            OsuLogin SafetyCenterResources ServiceConnectivityResources ServiceUwbResources ServiceWifiResources WifiDialog
+        )
+        apexes+=(
+            com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch
+            com.android.appsearch.apk com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast
+            com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt
+            com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.authsecret
+            com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual
+            com.android.hardware.boot com.android.hardware.cas com.android.hardware.neuralnetworks
+            com.android.hardware.rebootescrow com.android.hardware.wifi com.android.healthfitness
+            com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec
+            com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering
+            com.android.neuralnetworks com.android.nfcservices com.android.ondevicepersonalization com.android.os.statsd
+            com.android.permission com.android.profiling com.android.resolv com.android.rkpd com.android.runtime
+            com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer
+            com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb
+            com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor
+            com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal
+            com.google.pixel.vibrator.hal com.qorvo.uwb
+            # Added in 23
+            com.android.bt com.android.crashrecovery com.android.hardware.contexthub com.android.hardware.dumpstate
+            com.android.hardware.gatekeeper.nonsecure com.android.hardware.power com.android.hardware.thermal
+            com.android.hardware.threadnetwork com.android.hardware.uwb com.android.hardware.vibrator com.android.uprobestats
+            # Added in 23.2
+            com.android.telephonycore
+        )
+        ;;
     *)
         error "LineageOS $LINEAGE_VERSION is not supported"
         exit 2
